f7796e7993b5f9db6c1ca23df7228ce324e85b1f
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-04-05 13:34:54 +0000

    rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,15 +4,29 @@ export default class Counter extends Component {
 
     state = {
         count: 0,
+        tags: ['tags1', 'tag2', 'tag3', 'tag4']
     };
 
     render() {
+
+        let classes = this.getBadgeClasses()
+
         return (
         <>
-        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+            { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}            
+        </ul>
         </>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes; 
+
     }
 
     formatCount() {
